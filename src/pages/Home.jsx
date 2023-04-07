b138c0f16a8e38db14662cfc4cdc64f372771920
“dawood-abuzahra” <dabuzahra@pando-soft.com>
2023-04-07 23:30:38 +0300

    build

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import api from "../../src/api";
+import React from "react";
 import CardComp from "../components/Card/CardComp";
 
 const Home = (props) => {
